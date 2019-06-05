d3916e40e12ea697ae399a5af378fd27522d40aa
xialiu1988 <liuxia198896@gmail.com>
2019-06-04 23:57:16 -0700

    working on theater tests

---
+++ inheritance/src/test/java/inheritance/TheaterTest.java
@@ -0,0 +1,52 @@
+package inheritance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TheaterTest {
+
+    @Test
+    public void CangetName() {
+    }
+
+    @Test
+    public void CangetMovies() {
+    }
+
+    @Test
+    public void CangetMovieReviews() {
+    }
+
+    @Test
+    public void CansetName() {
+    }
+
+    @Test
+    public void CansetMovies() {
+    }
+
+    @Test
+    public void CanaddMovie() {
+    }
+
+    @Test
+    public void CanremoveMovie() {
+    }
+
+    @Test
+    public void CantoString() {
+    }
+
+    @Test
+    public void CangetReviews() {
+    }
+
+    @Test
+    public void CangetOneMovieReview() {
+    }
+
+    @Test
+    public void CanaddReview() {
+    }
+}
